1ef8d04c627ff652948b401e987943d75daf464e
Sahil Pagar <pagarsahil1@gmail.com>
2026-08-06 18:29:38 +0530

    setting up the routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.post('/city',CityController.create);
 //accessing the controller if a request is made to v1/city this is where its routed
+router.delete('/city/:id', CityController.destroy);
+
 
 module.exports = router;
